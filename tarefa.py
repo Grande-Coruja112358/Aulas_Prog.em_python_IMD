e6c4cdde2b038d0b4ff24195a1a8c176cbaf98de
Grande-Coruja112358 <maia.vinicius@escolar.ifrn.edu.br>
2026-08-17 20:35:07 -0300

    Create tarefa.py
    Still gotta finish work

---
+++ tarefa.py
@@ -0,0 +1,51 @@
+1)
+dia = 10
+mes = 7
+ano = 2000
+peso = 3.4
+sexo = 'M'
+print(' Dados do nascimento')
+print(f' Data: {dia}/{mes}/{ano}')
+print(f' Peso: {peso} Kg - Sexo: {sexo}')
+2)
+idade = int(input('Qual a sua idade? ')
+print(f' {idade + 10}')
+3) 
+R = int(input(' Digite o raio:')
+print(f' {3.14 * R ** 2}')
+4) 
+R = int(input(' Digite o raio:')
+print(f' {3.14 * R ** 2}')
+print(f' {2 * 3.14 * R}')
+5) 
+pro1 = int(input(' Digite o preço do 1º produto: ')
+pro2 = int(input(' Digite o preço do 2º produto: ')
+if pro1 > pro2:
+  print(f' {pro1} é maior que {pro2}')
+elif pro1 = pro2:
+  print(f' {pro1} é igual a {pro2}')
+elif pro1 < pro2:
+  print(f' {pro1} é menor que {pro2}')
+6) 
+num1 = int(input())
+num2 = int(input())
+print(f' Soma de {num1} e {num2} é {num1 + num2}')
+print(f' Subtração de {num1} e {num2} é {num1 - num2}')
+print(f' Mutiplicação de {num1} e {num2} é {num1 * num2}')
+print(f' Divisão de {num1} e {num2} é {num1 / num2}')
+7)
+valor1 = float(input())
+valor2 = float(input())
+valor3 = float(input())
+valor4 = float(input())
+print(f' {(valor1 + valor2 + valor3 + valor4) / 4}')
+if valor1 >= 7:
+  print(f' {valor1}')
+else:
+  print( 0 )
+8)
+temp = float(input())
+F = (C * 9) / 5 + 32
+print(f' {F}')
+9)
+email = input(' Digite o seu e-mail')
